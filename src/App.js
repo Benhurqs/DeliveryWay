14fff48f0e5f90478e10645812195975ee1e5a2c
Benhur_macbook <benhur.quintino@gmail.com>
2018-04-27 15:15:10 -0300

    Adicionando Maps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ZipField from './components/ZipField';
+import GoogleMapReact from 'google-map-react'
+
+const AnyReactComponent = ({ text }) => <div>{ text }</div>;
 
 class App extends Component {
+  static defaultProps = {
+    center: { lat: 40.7446790, lng: -73.9485420 },
+    zoom: 11
+  }
+
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to Delivery Way</h1>
+          <header className="App-header" >
+            <h1 className="App-title">Delivery Way</h1>
           </header>
           <div className="App-intro">
             <ZipField/>
+            <div className='google-map'>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: "AIzaSyAnFIvVKkyzjXMVdz7_z-jhDuAS2iT-8Ig" }}
+              defaultCenter={ this.props.center }
+              defaultZoom={ this.props.zoom }>
+              <AnyReactComponent
+                lat={ 40.7473310 }
+                lng={ -73.8517440 }
+                text={ "BLABLA" }
+              />
+            </GoogleMapReact>
           </div>
+          </div>
+          
         </div>
       </MuiThemeProvider>
     );
